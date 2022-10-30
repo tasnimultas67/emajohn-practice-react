1dc8366324a75b401386ca81434f504963928480
Tasnimul Haque <tasnimul.haque6@gmail.com>
2022-10-31 02:08:21 +0600

    just header and somthing mind

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import "./Header.css"
 
 const Header = () => {
     return (
         <div>
-            <h1>Hello Tasnimul From React Project Visual Studio Code</h1>
+        <div className='bg-gradient-to-r bg-black'>
+        <div className='header-container m-auto py-5 flex justify-between items-center'>
+                <div>
+                <h1 className='font-bold uppercase text-white border border-zinc-900 p-2 bg-black rotate-6'>Cut Me Off</h1>
+                </div>
+                <div>
+                    <ul className="flex">
+                        <li className="menu-list"><a href="#">Home</a></li>
+                        <li className="menu-list"><a href="#">Shop</a></li>
+                        <li className="menu-list"><a href="#">About</a></li>
+                        <li className="menu-list"><a href="#">Blog</a></li>
+                        <li className="menu-list"><a href="#">Contact</a></li>
+                        
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+        <h1 className='text-xl font-semibold w-100 from-cyan-500 to-blue-500 p-1 text-white '>Hello Tasnimul From React Project Visual Studio Code</h1>
         </div>
     );
 };
